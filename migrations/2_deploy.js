6d65fbce0145fc115f17909c3413853590cdb17f
Sahil Kharya <kharyasahil@gmail.com>
2021-04-10 00:48:42 +0530

    Setup react

--- migrations/2_deploy.js
+++ migrations/2_deploy.js
@@ -1,14 +1,5 @@
-const Token = artifacts.require("Token");
-const dBank = artifacts.require("dBank");
+const MediWallet = artifacts.require("MediWallet");
 
-module.exports = async function(deployer) {
-	//deploy Token
-
-	//assign token into variable to get it's address
-	
-	//pass token address for dBank contract(for future minting)
-
-	//assign dBank contract into variable to get it's address
-
-	//change token's owner/minter from deployer to dBank
+module.exports = function (deployer) {
+  deployer.deploy(MediWallet);
 };
--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,6 @@
 import { Tabs, Tab } from 'react-bootstrap'
-import dBank from '../abis/dBank.json'
 import React, { Component } from 'react';
-import Token from '../abis/Token.json'
-import dbank from '../dbank.png';
+import MediWallet from '../abis/MediWallet.json'
 import Web3 from 'web3';
 import './App.css';
 
@@ -15,7 +13,24 @@ class App extends Component {
   }
 
   async loadBlockchainData(dispatch) {
+    if(typeof window.ethereum!=='undefined'){
+      const web3 = new Web3(window.ethereum);
+      const netId = await web3.eth.net.getId();
+      const accounts = await web3.eth.getAccounts();
+      // console.log(accounts);
 
+      if(typeof accounts[0] !== 'undefined'){
+        const balance = await web3.eth.getBalance(accounts[0])
+        this.setState({account: accounts[0], balance: balance, web3: web3})
+      } else{
+        window.alert("Please login with Metamask")
+      }
+
+      new web3.eth.Contract(MediWallet.abi, accounts[0])
+
+    } else {
+      window.alert('Please install Metamask');
+    }
     //check if MetaMask exists
 
       //assign to values to variables: web3, netId, accounts
@@ -37,43 +52,66 @@ class App extends Component {
     //check if this.state.dbank is ok
     //in try block call dBank withdraw();
   }
-
+  myChangeHandler = (event) => {
+    console.log(event);
+    let nam = event.target.name;
+    let val = event.target.record;
+    // this.setState
+    console.log(this.state)
+  }
   constructor(props) {
     super(props)
     this.state = {
       web3: 'undefined',
       account: '',
-      token: null,
-      dbank: null,
       balance: 0,
-      dBankAddress: null
+      name: '',
+      record: ''
     }
   }
 
   render() {
     return (
       <div className='text-monospace'>
-        <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
-          <a
-            className="navbar-brand col-sm-3 col-md-2 mr-0"
-            href="http://www.dappuniversity.com/bootcamp"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-        <img src={dbank} className="App-logo" alt="logo" height="32"/>
-          <b>dBank</b>
-        </a>
+        <nav className="navbar fixed-top bg-primary flex-md-nowrap shadow">
+        <div className="row">
+          <div className="col-s6">
+          <b className="text-center">Medi Wallet</b>
+            
+          </div>
+          <div className="col-s6 text-right">
+          <b>{this.state.account}</b>
+
+          </div>
+        </div>
+     
         </nav>
         <div className="container-fluid mt-5 text-center">
         <br></br>
-          <h1>{/*add welcome msg*/}</h1>
-          <h2>{/*add user address*/}</h2>
+          <h1>Welcome to Medi Wallet</h1>
           <br></br>
           <div className="row">
             <main role="main" className="col-lg-12 d-flex text-center">
               <div className="content mr-auto ml-auto">
               <Tabs defaultActiveKey="profile" id="uncontrolled-tab-example">
-                {/*add Tab deposit*/}
+                <Tab eventKey="addRecord" title="addRecord">
+                  <div>
+                    Add new records
+                    <br></br><br></br>
+                    <form onSubmit={(e)=> {
+                      e.preventDefault();
+                      let name = this.nam.value;
+                    }}>
+                    <div className="form-group">
+                      <label>Name: <input id="name" name="name" class="form-control" onChange={this.myChangeHandler} required/></label>
+                    </div>
+                    <div className="form-group">
+                      <label>Record: <textarea id="record" name="record" value={this.state.value} class="form-control" onChange={this.myChangeHandler} required></textarea></label>
+                    </div>
+                    <button type="submit" className="btn btn-primary">Add</button>
+                    </form>
+                  </div>
+                </Tab>
                 {/*add Tab withdraw*/}
               </Tabs>
               </div>
